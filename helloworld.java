6bac91e56a13ce4d6128be227fa6c6e22cbae181
ianshajupadhyay <31827458+ianshajupadhyay@users.noreply.github.com>
2018-02-19 08:45:19 -0800

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    public static void main(String[] args){
+        Run();
+    }
+    public static void Run(){
+        System.out.println("Hello, World.");
+        
+        System.out.println("Hello, Java.");
+    }
+}
