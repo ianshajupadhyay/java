78f0ca79fd3623a0d8960bebf40e2b357de1c150
ianshajupadhyay <31827458+ianshajupadhyay@users.noreply.github.com>
2018-02-19 08:47:52 -0800

    Create stdinstdout2.java

---
+++ stdinstdout2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scan=new Scanner(System.in);
+       
+       int s = scan.nextInt();
+       double d = scan.nextDouble(); 
+      
+        scan.nextLine();
+        String st =scan.nextLine();
+       System.out.println("String: "+st ) ;
+       System.out.println("Double: "+d);
+       System.out.println("Int: "+s);
+      }
+}
