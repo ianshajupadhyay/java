76cd4ea021cdba130423689199980ae24887e4ba
ianshajupadhyay <31827458+ianshajupadhyay@users.noreply.github.com>
2018-02-19 08:46:51 -0800

    Create stdinandstdout.java

---
+++ stdinandstdout.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        {
+            int a = input.nextInt();
+            System.out.println(a);
+            int b=input.nextInt();
+            System.out.println(b);
+            int c = input.nextInt();
+            System.out.println(c);
+        }
+    }
+}
